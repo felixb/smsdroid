ac00aa42917545a3bbc0d4069a08aa7a3c6fa9f0
Felix Bechstein <f@ub0r.de>
2010-03-26 15:09:16 +0100

    cleanup

--- src/de/ub0r/android/smsdroid/WidgetProvider.java
+++ src/de/ub0r/android/smsdroid/WidgetProvider.java
@@ -1,78 +1,74 @@
 /*
- * Copyright (C) 2008 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright (C) 2010 Felix Bechstein
+ * 
+ * This file is part of SMSdroid.
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program; If not, see <http://www.gnu.org/licenses/>.
  */
-
 package de.ub0r.android.smsdroid;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
-import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
-import android.content.pm.PackageManager;
-import android.os.SystemClock;
 import android.util.Log;
 import android.widget.RemoteViews;
 
 /**
- * A widget provider.  We have a string that we pull from a preference in order to show
- * the configuration settings and the current time when the widget was updated.  We also
- * register a BroadcastReceiver for time-changed and timezone-changed broadcasts, and
- * update then too.
- *
- * <p>See also the following files:
- * <ul>
- *   <li>ExampleAppWidgetConfigure.java</li>
- *   <li>ExampleBroadcastReceiver.java</li>
- *   <li>res/layout/appwidget_configure.xml</li>
- *   <li>res/layout/appwidget_provider.xml</li>
- *   <li>res/xml/appwidget_provider.xml</li>
- * </ul>
+ * A widget provider.
  */
-public class WidgetProvider extends AppWidgetProvider {
-    // log tag
-    private static final String TAG = "SMSdroid.wdp";
+public final class WidgetProvider extends AppWidgetProvider {
+	/** Tag for output. */
+	private static final String TAG = "SMSdroid.wdp";
 
-    @Override
-    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        Log.d(TAG, "onUpdate");
-	final Cursor cursor = context.getContentResolver().query(SmsReceiver.URI,
-			MessageListAdapter.PROJECTION,
-			MessageListAdapter.SELECTION_UNREAD, null, null);
-	final int l = cursor.getCount();
-	Log.d(TAG, "l: " + l);
-        final int N = appWidgetIds.length;
-        for (int i=0; i<N; i++) {
-		RemoteViews views = getRemoteViews(context);
-        	appWidgetManager.updateAppWidget(appWidgetId, views);
-        }
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void onUpdate(final Context context,
+			final AppWidgetManager appWidgetManager, final int[] appWidgetIds) {
+		Log.d(TAG, "onUpdate");
+
+		final int n = appWidgetIds.length;
+		for (int i = 0; i < n; i++) {
+			RemoteViews views = getRemoteViews(context);
+			appWidgetManager.updateAppWidget(i, views);
+		}
+	}
 
 	/**
 	 * Get {@link RemoteViews}.
-	 * @package context {@link Context}
+	 * 
+	 * @param context
+	 *            {@link Context}
 	 * @return {@link RemoteViews}
 	 */
-    static RemoteViews getRemoteViews(final Context context) {
-        Intent intent = new Intent(context, SMSdroid.class);
-	PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
-	int appWidgetId = appWidgetIds[i];
-       	RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget);
-	views.setTextViewText(R.id.text1, String.valueOf(l));
-	views.setOnClickPendingIntent(R.id.button, pendingIntent);
-	return views;
-    }
+	static RemoteViews getRemoteViews(final Context context) {
+		Intent intent = new Intent(context, SMSdroid.class);
+		PendingIntent pendingIntent = PendingIntent.getActivity(context, 0,
+				intent, 0);
+		RemoteViews views = new RemoteViews(context.getPackageName(),
+				R.layout.widget);
+		final Cursor cursor = context.getContentResolver().query(
+				SmsReceiver.URI, MessageListAdapter.PROJECTION,
+				MessageListAdapter.SELECTION_UNREAD, null, null);
+		final int l = cursor.getCount();
+		Log.d(TAG, "l: " + l);
+		views.setTextViewText(R.id.text1, String.valueOf(l));
+		views.setOnClickPendingIntent(R.id.text1, pendingIntent);
+		return views;
+	}
 }
-
