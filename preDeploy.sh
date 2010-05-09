06bc057f7da623efe4418334b42c0fe16b9d2f16
Felix Bechstein <f@ub0r.de>
2010-05-09 23:25:37 +0200

    fix deploy scripts

--- preDeploy.sh
+++ preDeploy.sh
@@ -7,8 +7,12 @@ else
 fi
 
 for f in $(find "${p}" -name \*java) ; do
-	sed -e 's:Log.v://Log.v:' -i $f
-	sed -e 's:Log.d://Log.d:' -i $f
+	sed \
+		-e 's: Log.v: //Log.v:' \
+		-e 's: Log.d: //Log.d:' \
+		-e 's:\tLog.v:\t//Log.v:' \
+		-e 's:\tLog.d:\t//Log.d:' \
+		-i $f
 done
 
 sed -e 's/android:debuggable="true"/android:debuggable="false"/' -i AndroidManifest.xml
